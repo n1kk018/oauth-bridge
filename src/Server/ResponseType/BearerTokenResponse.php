fa0f3b2a39952510d11a0c5c2d53914362a4928d
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-15 01:15:32 +0300

    Added BearerTokenResponse

---
+++ src/Server/ResponseType/BearerTokenResponse.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Preferans\Oauth\Server\ResponseType;
+
+use DateTime;
+use Phalcon\Http\ResponseInterface;
+use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
+
+/**
+ * Preferans\Oauth\Server\ResponseType\BearerTokenResponse
+ *
+ * @package Preferans\Oauth\Server\ResponseType
+ */
+class BearerTokenResponse extends AbstractResponseType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function generateHttpResponse(ResponseInterface $response)
+    {
+        $expireDateTime = $this->accessToken->getExpiryDateTime()->getTimestamp();
+        $jwtAccessToken = $this->accessToken->convertToJWT($this->privateKey);
+
+        $responseParams = [
+            'token_type'   => 'Bearer',
+            'expires_in'   => $expireDateTime - (new DateTime())->getTimestamp(),
+            'access_token' => (string)$jwtAccessToken,
+        ];
+
+        if ($this->refreshToken instanceof RefreshTokenEntityInterface) {
+            $refreshToken = $this->encrypt(
+                json_encode(
+                    [
+                        'client_id'        => $this->accessToken->getClient()->getIdentifier(),
+                        'refresh_token_id' => $this->refreshToken->getIdentifier(),
+                        'access_token_id'  => $this->accessToken->getIdentifier(),
+                        'scopes'           => $this->accessToken->getScopes(),
+                        'user_id'          => $this->accessToken->getUserIdentifier(),
+                        'expire_time'      => $this->refreshToken->getExpiryDateTime()->getTimestamp(),
+                    ]
+                )
+            );
+
+            $responseParams['refresh_token'] = $refreshToken;
+        }
+
+        $responseParams = array_merge($this->getExtraParams($this->accessToken), $responseParams);
+
+        $response = $response
+            ->setStatusCode(200)
+            ->setHeader('pragma', 'no-cache')
+            ->setHeader('cache-control', 'no-store')
+            ->setJsonContent($responseParams);
+
+        return $response;
+    }
+
+    /**
+     * Add custom fields to your Bearer Token response here, then override
+     * AuthorizationServer::getResponseType() to pull in your version of
+     * this class rather than the default.
+     *
+     * @param AccessTokenEntityInterface $accessToken
+     *
+     * @return array
+     */
+    protected function getExtraParams(AccessTokenEntityInterface $accessToken)
+    {
+        return [];
+    }
+}
