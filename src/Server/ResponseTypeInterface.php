3b5af8a76b21db424fce4b229d59590dc7f88bb3
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-15 00:25:59 +0300

    Added ResponseTypeInterface

---
+++ src/Server/ResponseTypeInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Preferans\Oauth\Server;
+
+use Phalcon\Http\ResponseInterface;
+use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
+
+/**
+ * Preferans\Oauth\Server\ResponseTypeInterface
+ *
+ * @package Preferans\Oauth\Server
+ */
+interface ResponseTypeInterface
+{
+    /**
+     * @param AccessTokenEntityInterface $accessToken
+     */
+    public function setAccessToken(AccessTokenEntityInterface $accessToken);
+
+    /**
+     * @param RefreshTokenEntityInterface $refreshToken
+     */
+    public function setRefreshToken(RefreshTokenEntityInterface $refreshToken);
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    public function generateHttpResponse(ResponseInterface $response);
+
+    /**
+     * Set the encryption key
+     *
+     * @param string|null $key
+     */
+    public function setEncryptionKey($key = null);
+}
