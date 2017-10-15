9d4dbfcc21ee7e870db8378bc0ee3000fcf5703e
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-15 12:04:49 +0300

    Added ClientCredentials Grant

---
+++ src/Server/Grant/ClientCredentialsGrant.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Preferans\Oauth\Server\Grant;
+
+use DateInterval;
+use Phalcon\Http\RequestInterface;
+use Preferans\Oauth\Server\ResponseTypeInterface;
+
+/**
+ * Preferans\Oauth\Server\Grant\ClientCredentialsGrant
+ *
+ * @package Preferans\Oauth\Server\Grant
+ */
+class ClientCredentialsGrant extends AbstractGrant
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function respondToAccessTokenRequest(
+        RequestInterface $request,
+        ResponseTypeInterface $responseType,
+        DateInterval $accessTokenTTL
+    ) {
+        // Validate request
+        $client = $this->validateClient($request);
+        $scopes = $this->validateScopes($this->getRequestParameter('scope', $request));
+
+        // Finalize the requested scopes
+        $scopes = $this->scopeRepository->finalizeScopes($scopes, $this->getIdentifier(), $client);
+
+        // Issue and persist access token
+        $accessToken = $this->issueAccessToken($accessTokenTTL, $client, null, $scopes);
+
+        // Inject access token into response type
+        $responseType->setAccessToken($accessToken);
+
+        return $responseType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier()
+    {
+        return 'client_credentials';
+    }
+}
